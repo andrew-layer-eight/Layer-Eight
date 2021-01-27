381320a9e036c3f2cc0ec4b18c9eb8484d2626ba
andrew-layer-eight <69631363+andrew-layer-eight@users.noreply.github.com>
2021-01-27 16:13:47 +0000

    mac-finder regex complete

--- mac-address-finder.py
+++ mac-address-finder.py
@@ -1,11 +1,12 @@
 from netmiko import ConnectHandler #connecthandler from netmiko library
 import re #regular expressions
-#from getpass import getpass #password handler do this later
-from threading import Thread # so i can run commands in parallel, this looks overly complicated at the moment... I'll leave it imported but wont use it. 
-from datetime import datetime #timestamps for delay factor of ssh, again not used yet. 
-starttime = datetime.now() # to do with threading.. not really needed
+from getpass import getpass #password handler do this later
+from getpass import getuser
 
 print("Welcome to MAC finder, this will search all switches in the network for a device MAC")
+
+#--------------------------------- Start of Functions ---------------------------------#
+
 # convert the mac address to Cisco standard xxxx.xxxx.xxxx
 def cisco_mac(in_item): 
     my_mac = [] 
@@ -17,67 +18,105 @@ def cisco_mac(in_item):
         else:
             my_mac.append(item)
         index += 1
-    return "".join(my_mac) #
+    return "".join(my_mac) 
 
+#def switch_commands:
+
+#def connect_devices:
+
+#
+
+#--------------------------------- End of Functions ---------------------------------#
+
+#--------------------------------- File's with IP addresses of devices per region ---------------------------------#
+europe_ip_addr_file = open("ip-europe.txt")
+europe_ip_addrs = europe_ip_addr_file.read().splitlines()
+london_ip_addr_file = open("ip-london.txt")
+london_ip_addrs = europe_ip_addr_file.read().splitlines()
+denver_ip_addr_file = open("ip-denver.txt")
+denver_ip_addrs = europe_ip_addr_file.read().splitlines()
+us_ip_addr_file = open("ip-us.txt")
+us_ip_addrs = europe_ip_addr_file.read().splitlines()
+asia_ip_addr_file = open("ip-asia.txt")
+asia_ip_addrs = europe_ip_addr_file.read().splitlines()
+aus_ip_addr_file = open("ip-aus.txt")
+aus_ip_addrs = europe_ip_addr_file.read().splitlines()
+
+#---------------- Dictionary ------------------#
+location = {
+    "London": london_ip_addr_file
+}
+
+print("valid locations are: London, Denver, Europe, Asia, USA, Australia")
+location = input("Which location are you in: ")
 user_mac = input("What is the MAC you're searching for: ").lower() # ask user for mac, make it lower case
 in_mac = [letter for letter in user_mac if letter.isalnum()] #  remove all non alphanumeric characters, create a new variable 
 mac_cisco = (cisco_mac(in_mac)) # create a variable from the function and user input
+
+
+
+username = getuser() #this takes the current logged in user - will need changing if using a different account to  log in. 
+password = getpass()
+
 
 # create a dictionary for devices. this can be improved... 
 device_a = {
     "device_type": "cisco_ios_telnet",
     "ip": '192.168.1.100',
-    "username": "admin",
-    "password": "password",
+    "username": username,
+    "password": password,
     "port" : 32770, # only included as i was using EVE-NG which does port forwarding on Telnet. 
     "global_delay_factor": 3 # allows 300 seconds for command to complete. 
 }
 device_b = {
     "device_type": "cisco_ios_telnet",
     "ip": '192.168.1.100',
-    "username": "admin",
-    "password": "password",
+    "username": username,
+    "password": password,
     "port" : 32773, 
     "global_delay_factor": 3
 }
-all_devices = [device_a, device_b]
+all_devices = [device_a] #device_b]
 """ This section is in progress, to work with above... find out how not to store creds in script and also to reference an IP/port list. 
 ipfile = open("ipaddresses.txt")
-#ask user for creds to login 
-print("I need your credentials to login to the switches, sorry I'm not that clever.")
-all_devices["username"] = input("What's your Username? ")
-all_devices["password"] = input("What's the Password? ")
 """
+
+#--------------------------------- Regex ---------------------------------#
+interface_regex = re.compile(r"(?:GigabitEthernet|TenGigabitEthernet|FastEthernet|FortyGigabitEthernet|Eth)\d+((/\d+)+(\.\d+)?)?")
+access_regex = re.compile(r"(\sOperational\sMode\:\saccess)")
+
+#--------------------------------- Start of Job ---------------------------------#
 for devices in all_devices:
     connect = ConnectHandler(**devices) #ssh to the devices using the dictionary above
     output = connect.send_command("show mac address-table | inc " + str(mac_cisco)) #now look for the MAC on these switches
     if mac_cisco in output: 
-        var_interface = output.split()
-        interface_regex = re.search(r"(?:GigabitEthernet|TenGigabitEthernet|FastEthernet|FortyGigabitEthernet)\d+((/\d+)+(\.\d+)?)?)", var_interface)
-       # interface = var_interface[7] # get the interface this MAC is on (Rollback if needed)
-       # check the interface is in Access Mode Cisco IOS-XE
-        sh_int = connect.send_command("show interface " + interface_regex + " switchport") #look at the interface details
-        access_port = "Administrative Mode: static access"
-        #x = sh_int.splitlines() 
-        #access_port = x[6] #find the "port mode" line - Change this to Regex
-        #if "access" in access_port: #if "access" as port-mode, do the below
-        if re.findall(r'"(.*?)"', access_port) in sh_int:
-            # get the hostname from the CORRECT device
-            hostname = connect.send_command("show run | i hostname")
-            hostname1 = hostname.split()
-            cisco_hostname = hostname1[1] 
-            print(f"This MAC: {mac_cisco} is on this device: {cisco_hostname} and this port: {interface_regex}") 
-            break
-        else: # if port-mode doesn't have access, continue until it finds it. Layer 2 environments
-            continue
+        # Turn this into a function!!! 
+        for line_item in output.splitlines():
+            if mac_cisco in line_item:       
+                var_int = interface_regex.search(line_item)
+                interface = var_int.group()
+                sh_int = connect.send_command("show interface " + interface + " switchport") #look at the interface details
+                #access_port = "Operational Mode: access"
+                x = sh_int.splitlines()
+                for port_info in sh_int.splitlines():
+                    var_mode = access_regex.search(port_info)
+                    if var_mode is not None:
+                         # get the hostname from the CORRECT device
+                        hostname = connect.send_command("show run | i hostname")
+                        hostname1 = hostname.split()
+                        cisco_hostname = hostname1[1] 
+                        print(f"This MAC: {mac_cisco} is on this device: {cisco_hostname} and this port: {interface}") 
+                        break
+                    else: # if port-mode doesn't have access, continue until it finds it. Layer 2 environments
+                        continue
+            else:
+                break
     else:
-        print("sorry can't find this MAC!")
+        print("sorry can't find this MAC! - have you typed it correctly?")
 
-print("MAC Search Engine has now Completed")
+print("MAC Finder has now completed")
 
-#exit from devices.
+#--------------------------------- disconnect ---------------------------------#
 connect.disconnect()
 
-# regex - (?:GigabitEthernet|TenGigabitEthernet|FastEthernet|FortyGigabitEthernet)\d+((/\d+)+(\.\d+)?)?
-
-# add search location EUROPE, LONDON etc then use a different list for those locations. 
+# add search location EUROPE, LONDON etc then use a different list for those locations
