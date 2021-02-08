b16655fe853165214b57719d0c5873534c1fff11
andrew-layer-eight <69631363+andrew-layer-eight@users.noreply.github.com>
2021-02-08 14:17:57 +0000

    vpn template complete

--- VPN-Template/ikev2-vpn-form.py
+++ VPN-Template/ikev2-vpn-form.py
@@ -9,13 +9,28 @@ template = env.get_template('ikev2-temp.txt')
 # variables in the VPNs
 party_name = input("Who is this for(3rd party)? ").upper().replace(" ", "")
 domain = input("What is this envrionment(prod,dev,test)? ").upper().replace(" ", "")
+dc = input("remote side DC? ")
 vpn_name = party_name + "_" + domain
 peer_pub_ip = input("What is their peer IP? ")
-local_pub_ip = input("What is our Peer IP? ")
 peer_lan_ip = input("what is their LAN IP? ")
+local_pub_ip = input("What is our Peer IP? ")
 psk = input("please type the PSK: ")
+tunnel_int = int(input("what tunnel interface number? "))
+tunnel_ip = input("what is the IP of the tunnel interface? ")
+ike_lifetime = int(input("what is the phase 1 lifetime? "))
+ipsec_lifetime = int(input("Phase 2 sa lifetime? "))
+#make htis an if statement
+pfs = input("is pfs required? (yes/no) ")
+if pfs == "yes":
+    pfs2 = int(input("PFS group? "))
+else:
+    pfs2 = "none"
 
 
-output = template.render(vpn_name=vpn_name, peer_gw=peer_pub_ip)
-
+# apply the variables to the template
+output = template.render(vpn_name=vpn_name, peer_gw=peer_pub_ip, key=psk, local_peer=local_pub_ip, tunnel_int=tunnel_int, tunnel_ip=tunnel_ip, ipsec_lifetime=ipsec_lifetime, pfs2=pfs2, dc=dc)
 print(output)
+# write the output to a txt file so can copy and paste it. 
+file = open("vpn_config.txt", "w")
+file.write(output)
+file.close()
