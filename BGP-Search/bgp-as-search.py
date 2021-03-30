1d63ae8016ae5575d2ca8143cee0b128dad580f9
andrew-layer-eight <69631363+andrew-layer-eight@users.noreply.github.com>
2021-03-30 16:39:01 +0100

    update

--- BGP-Search/bgp-as-search.py
+++ BGP-Search/bgp-as-search.py
@@ -1,26 +1,26 @@
-# this script will search internal devices for BGP AS Numbers and print a list of ones in use. 
+from getpass import getpass #password handler do this later
+from getpass import getuser
 from netmiko import ConnectHandler #connecthandler from netmiko library
-device_a = {
-    "device_type": "cisco_ios_telnet",
-    "ip": '192.168.1.100',
-    "username": "admin",
-    "password": "cisco",
-    "port" : 32769, # only included as i was using EVE-NG which does port forwarding on Telnet. 
-    "global_delay_factor": 3 # allows 300 seconds for command to complete. 
-}
-device_b = {
-    "device_type": "cisco_ios_telnet",
-    "ip": '192.168.1.100',
-    "username": "admin",
-    "password": "cisco",
-    "port" : 32776, 
+
+#open file for output
+target = open("as-path-output.txt", 'w+')
+
+username = getpass()
+password = getpass()
+
+# insert dictionary including all IP's you want to check here
+all_devices = ["10.1.1.1", "10.208.1.1", "10.11.1.1"] 
+
+ # run commands on devices
+for ip in all_devices:
+    device = {
+    "device_type": "cisco_iosxe",
+    "ip": ip,
+    "username": username,
+    "password": password,
     "global_delay_factor": 3
 }
-all_devices = [device_a, device_b]
-
-# run commands on devices
-for devices in all_devices:
-    connect = ConnectHandler(**devices) #ssh to the devices using the dictionary above
+    connect = ConnectHandler(**device) #ssh to the devices using the dictionary above
     output = connect.send_command("show ip bgp summary") # find the AS numbers
     bgp_sum = output.split()
     as_number = bgp_sum[7]
@@ -28,17 +28,20 @@ for devices in all_devices:
     hostname1 = hostname.split()
     cisco_hostname = hostname1[1]
     print("BGP AS", as_number, "Is on:", cisco_hostname) # print the hostname and BGP AS Correlation
+    target.write("BGP AS", as_number, "Is on:", cisco_hostname)
 
- # add a section to search for a specific AS Number
-    print("Are you looking for a specific BGP AS Number?")
-    input("Y/N: ")
-    if input == 'Y' or 'y':
-        as_num = input("What AS Number? ")
-        find_as_number = as_number.find("local AS number", as_num)
-        print(find_as_number)
-    else: 
-        print("Ok, I'll get you a list of ALL the AS Numbers i know about")
+# add a section to search for a specific AS Number
+print("Are you looking for a specific BGP AS Number?")
+input("Y/N: ")
+if input == 'Y' or 'y':
+    as_num = input("What AS Number? ")
+    find_as_number = as_num.find("local AS number", as_num)
+    print(find_as_number)
+else: 
+    print("Ok, I'll get you a list of ALL the AS Numbers i know about")
+
         
            
-# lets move this to be done via an API if that fails then run SSH.
 
+#--------------------------------- disconnect ---------------------------------#
+connect.disconnect()
